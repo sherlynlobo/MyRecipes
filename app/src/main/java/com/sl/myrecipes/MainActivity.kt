45ca9a85f0e5054f156be60cec9167ed889ab392
sherlynlobo <sherlynlobo26@gmail.com>
2021-01-22 15:56:10 -0500

    Initial commit

---
+++ app/src/main/java/com/sl/myrecipes/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.sl.myrecipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun goToRecipe(view: View) {
+        Toast.makeText(this@MainActivity,R.string.word, Toast.LENGTH_LONG).show()
+    }
+}
