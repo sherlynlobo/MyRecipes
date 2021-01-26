130bd239528a532ba567bbd5757cd12b18cd69fb
sherlynlobo <sherlynlobo26@gmail.com>
2021-01-25 19:55:03 -0500

    SubCategoryAdapter

---
+++ app/src/main/java/com/sl/myrecipes/adapter/SubCategoryAdapter.kt
@@ -0,0 +1,33 @@
+package com.sl.myrecipes.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sl.myrecipes.R
+import com.sl.myrecipes.entities.Recipes
+import kotlinx.android.synthetic.main.item_rv_sub_category.view.*
+
+class SubCategoryAdapter: RecyclerView.Adapter<SubCategoryAdapter.RecipeViewHolder>() {
+
+    var arrSubCategory = ArrayList<Recipes>()
+    class RecipeViewHolder(view:View): RecyclerView.ViewHolder(view){
+
+    }
+    fun setData(arrData: List<Recipes>){
+        arrSubCategory = arrData as ArrayList<Recipes>
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        return RecipeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_rv_sub_category,parent,false))
+
+    }
+
+    override fun getItemCount(): Int {
+        return arrSubCategory.size
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        holder.itemView.tv_dish_name.text = arrSubCategory[position].dishNAme
+        }
+
+}
