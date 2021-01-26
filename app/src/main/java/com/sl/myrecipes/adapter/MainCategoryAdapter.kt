22d0f0044f673caab4976394cfd29be7201f7d7d
sherlynlobo <sherlynlobo26@gmail.com>
2021-01-25 19:53:00 -0500

    MainCategoryAdapter

---
+++ app/src/main/java/com/sl/myrecipes/adapter/MainCategoryAdapter.kt
@@ -0,0 +1,33 @@
+package com.sl.myrecipes.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sl.myrecipes.R
+import com.sl.myrecipes.entities.Recipes
+import kotlinx.android.synthetic.main.item_rv_sub_category.view.*
+
+class MainCategoryAdapter: RecyclerView.Adapter<MainCategoryAdapter.RecipeViewHolder>() {
+
+    var arrMainCategory = ArrayList<Recipes>()
+    class RecipeViewHolder(view:View): RecyclerView.ViewHolder(view){
+
+    }
+    fun setData(arrData: List<Recipes>){
+        arrMainCategory = arrData as ArrayList<Recipes>
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        return RecipeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_rv_main_category,parent,false))
+
+    }
+
+    override fun getItemCount(): Int {
+        return arrMainCategory.size
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        holder.itemView.tv_dish_name.text = arrMainCategory[position].dishNAme
+        }
+
+}
